4e6ef9065cd02aafef3571e7d40cf08c9b8fc286
Jacob Mellor <developers@ironsoftware.com>
2018-09-21 12:41:27 +0700

    Initial COmmit
    Initial commit - IronPDF reference updated to latest
    
    Read teh full walkthrough at :
    
    https://ironpdf.com/tutorials/vb-net-pdf/

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports IronPdf
+
+Module Module1
+
+    Sub Main()
+        Dim renderer = New HtmlToPdf()
+        renderer.PrintOptions.CssMediaType = PdfPrintOptions.PdfCssMediaType.Print
+        renderer.PrintOptions.PrintHtmlBackgrounds = False
+        renderer.PrintOptions.PaperOrientation = PdfPrintOptions.PdfPaperOrientation.Landscape
+        renderer.PrintOptions.RenderDelay = 150
+
+
+        renderer.PrintOptions.Header.CenterText = "VB.Net PDF Slideshow"
+        renderer.PrintOptions.Header.DrawDividerLine = True
+        renderer.PrintOptions.Header.FontSize = "13"
+
+        renderer.PrintOptions.Footer.RightText = "page {page} of {total-pages}"
+        renderer.PrintOptions.Footer.FontFamily = "Arial"
+        renderer.PrintOptions.Footer.FontSize = "9"
+
+
+        Dim document = renderer.RenderHTMLFileAsPdf("..\..\slideshow\index.html")
+        document.SaveAs("Html5WithHeader.pdf")
+        System.Diagnostics.Process.Start("Html5WithHeader.pdf")
+    End Sub
+
+End Module
